84223fec4b360fab5d523a22a77285cac21541d3
ngthduy <39298854+ngthduy@users.noreply.github.com>
2019-08-13 11:05:04 +0700

    Add files via upload

---
+++ includes/database.php
@@ -0,0 +1,16 @@
+﻿<?php
+
+$hostname = "localhost";
+$dbuser = "root";
+$dbpassword = "";
+$dbname = "dangbaoche";
+
+//Kết nối đến CSDL
+$db = mysqli_connect($hostname, $dbuser, $dbpassword, $dbname);
+if (mysqli_connect_error()) {
+	die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+}
+// Change character set to utf8
+mysqli_set_charset($db, "utf8");
+
+?>
